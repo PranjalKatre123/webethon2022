f8b6578693c99e8a0c96353e31d55700a52694c5
Pranjal Katre <pranjalkatre@gmail.com>
2022-03-28 22:54:28 +0530

    adding second diff file

---
+++ diff.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class diff
+{
+    public static void main(Strings[] args)
+    {
+        int s=10-20;
+        System.out.println("diff of two integers 10 and 12 is = "+s);
+    }
+}
