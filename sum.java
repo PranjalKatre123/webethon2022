b091a2a20573748e8efd237f52708e90a2ed2fc5
Pranjal Katre <pranjalkatre@gmail.com>
2022-03-28 22:58:34 +0530

    all the file are ommited--3rd commit

--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public class sum
     public static void main(Strings[] args)
     {
         int s=10+20;
-        System.out.println("sum= "+s);
+        System.out.println("sum of two integers 10 and 12 is = "+s);
     }
 }
